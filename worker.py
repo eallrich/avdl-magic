248c0915fbc8467a528eeeb715fab629edc0c85f
Evan Allrich <evan@unguku.com>
2015-10-19 21:47:16 -0500

    Quick cleanup

--- worker.py
+++ worker.py
@@ -1,12 +1,11 @@
-import os
-
 import redis
 from rq import Worker, Queue, Connection
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-conn = redis.from_url(redis_url)
+import settings
+
+redis = redis.from_url(settings.redis_url)
 
 if __name__ == '__main__':
-    with Connection(conn):
+    with Connection(redis):
         worker = Worker([Queue('default')])
         worker.work()
