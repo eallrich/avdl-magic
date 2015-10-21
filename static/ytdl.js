0ea28f87f45af661d3e8bf8be6f46c9a4b05dbf2
Evan Allrich <evan@unguku.com>
2015-10-20 22:55:27 -0500

    Basic watcher

--- static/ytdl.js
+++ static/ytdl.js
@@ -1,16 +1,21 @@
 var ytdlApp = angular.module('ytdlApp', []);
 
-ytdlApp.controller('ytdlController', ['$scope', '$log', '$http', function($scope, $log, $http) {
+ytdlApp.controller('ytdlController', ['$scope', '$log', '$http', '$timeout', function($scope, $log, $http, $timeout) {
   $scope.watchQueue = function() {
-    $log.log('watchQueue called')
+    $log.log('watchQueue triggered');
+    var watcher = function() {
+      $http.get('/queued').
+        success(function(jobs) {
+          $log.log("Found " + jobs.length + " jobs queued");
+          $timeout(watcher, 3000); // 3 seconds
+        }).
+        error(function(error) {
+          $log.log(error);
+        });
+    };
 
-    $http.get('/queued').
-      success(function(jobs) {
-        $log.log(jobs);
-      }).
-      error(function(error) {
-        $log.log(error);
-      });
+    watcher();
   };
+
 }]);
 
