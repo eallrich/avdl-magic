6102f350f9e94026f5d3a44a1d405f2c206ccc53
Evan Allrich <evan@unguku.com>
2015-10-20 22:27:51 -0500

    Initial  use. Add ng-app name

--- static/ytdl.js
+++ static/ytdl.js
@@ -7,6 +7,14 @@
   .controller('ytdlController', ['$scope', '$log', '$http', function($scope, $log, $http) {
     $scope.watchQueue = function() {
       $log.log('watchQueue called')
+
+      $http.get('/queued').
+        success(function(jobs) {
+          $log.log(jobs);
+        }).
+        error(function(error) {
+          $log.log(error);
+        });
     };
   }
 
