ee9a97a92ce9db8d1a066e721f541fcb195ad640
Evan Allrich <evan@unguku.com>
2015-10-21 19:41:05 -0500

    Try to get fancy with timers

--- static/ytdl.js
+++ static/ytdl.js
@@ -9,23 +9,31 @@ ytdlApp.controller('ytdlController',
         $http.post('/api/enqueue', {'yturl': $scope.yturl}).
             success(function(response) {
                 $log.log(response);
-                watcher();
+                watcher(900); // milliseconds
             }).
             error(function(error) {
                 $log.log(error);
             });
     };
 
-    var watcher = function() {
-        $log.log('watcher triggered');
+    var watcher = function(delay) {
+        $log.log('watcher triggered with delay of ' + delay + "ms");
         var watchStatus = function() {
             $http.get('/api/status').
                 success(function(data) {
                     $scope.jobs = data.jobs
                     $scope.downloaded = data.files
-                    $log.log("Found " + $scope.jobs.length + " jobs queued");
-                    $log.log("Found " + $scope.downloaded.length + " downloaded files");
-                    //$timeout(watchStatus, 3000); // milliseconds
+                    var active_jobs = false;
+                    for(var i = 0; i < $scope.jobs.length; i++) {
+                        var job = $scope.jobs[i];
+                        if(job.status === "status" || job.status === "queued") {
+                            active_jobs = true;
+                            break;
+                        }
+                    }
+                    if(active_jobs && delay > 0) {
+                        $timeout(watchStatus, delay);
+                    }
                 }).
                 error(function(error) {
                     $log.log(error);
@@ -35,7 +43,8 @@ ytdlApp.controller('ytdlController',
         watchStatus();
     };
 
-    watcher();
+    // Initialize queue & download status
+    watcher(0); // don't poll
 
 }]);
 
