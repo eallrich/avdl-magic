7fb24b78d8517bce9c63af85b838d1ddbf83da03
Evan Allrich <evan@unguku.com>
2017-10-12 22:01:54 -0500

    Document fix

--- util.py
+++ util.py
@@ -56,6 +56,9 @@ def download(url):
     # Then get the video
     options = [
         'youtube-dl',
+        # Careful with these options. The worker has trouble when the args
+        # contain spaces (e.g. --opt foo). Instead, use an equals sign between
+        # the arg name and the value (e.g. --opt=foo).
         '--ffmpeg-location=/app/ffmpeg/',
         '--default-search=ytsearch:',
         '--restrict-filenames',
